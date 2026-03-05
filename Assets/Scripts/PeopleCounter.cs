1ecaaaff697e7fc103e80e19fc6aa7a032f9bb6e
NotTanos <rafaelbjarquin@gmail.com>
2026-03-04 21:50:02 -0500

    Added People Counter + Text

---
+++ Assets/Scripts/PeopleCounter.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+
+public class PeopleCounter : MonoBehaviour
+
+{
+    public int peopleCount = 0;
+    public TMPro.TextMeshProUGUI countText;
+
+    public void UpdatePeopleText()
+    {
+        if(countText != null)
+        {
+            countText.text = "There are " + peopleCount.ToString() + " people on this planet.";
+        }
+    }
+
+    public void AddToCount(int amount)
+    {
+        peopleCount += amount;  
+        UpdatePeopleText();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UpdatePeopleText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
