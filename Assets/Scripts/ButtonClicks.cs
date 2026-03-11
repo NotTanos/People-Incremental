fb9773e4cd04d974b6cc55d1a5a1598d28dc17c3
NotTanos <rafaelbjarquin@gmail.com>
2026-03-11 16:35:40 -0400

    Added new Litter Upgrade button
    doubles the amount of humans made on every level. grows exponentially

--- Assets/Scripts/ButtonClicks.cs
+++ Assets/Scripts/ButtonClicks.cs
@@ -5,15 +5,17 @@ using TMPro;
 public class ButtonClicks : MonoBehaviour
 {
 
-    public Button button1, forceReproduceButton, fertilityUpgrade1;
-    public TMPro.TextMeshProUGUI button1Text, forceReproduceText, fertilityUpgrade1Text;
+    public Button button1, forceReproduceButton, fertilityUpgrade1, litterUpgrade;
+    public TMPro.TextMeshProUGUI button1Text, forceReproduceText, fertilityUpgrade1Text, litterUpgradeText, fertilityUpgradePanelText, litterUpgradePanelText;
 
     public PeopleCounter peopleCounter;
 
     Coroutine autoPpl;
     int Button1ClickedAmnt = 0;
 
-    int fertilityUpgrade1Level = 1;
+    int fertilityUpgrade1Level = 0;
+    int litterUpgradeLevel = 0;
+    double litterUpgradeCost = 250;
     
 
     /* 
@@ -33,6 +35,7 @@ public class ButtonClicks : MonoBehaviour
             button1.gameObject.SetActive(false);
             forceReproduceButton.gameObject.SetActive(true);
             fertilityUpgrade1.gameObject.SetActive(true);
+            litterUpgrade.gameObject.SetActive(true);
 
             autoPpl = StartCoroutine(peopleCounter.RepeatAdd(1, 5.0f));
         }
@@ -41,35 +44,62 @@ public class ButtonClicks : MonoBehaviour
     // I think this one is pretty self explanatory...
     void ForceReproduceClicked() 
     {
-        peopleCounter.AddToCount(1);
+        peopleCounter.AddToCount((double)(1*Mathf.Pow(2, litterUpgradeLevel)));
     }
 
     void FertilityUpgradeClicked()
     {
-        double fertilityUpgrade1Cost = (double)Mathf.Pow(100, fertilityUpgrade1Level);
+        double fertilityUpgrade1Cost = (double)Mathf.Pow(100, fertilityUpgrade1Level+1);
 
-        if(peopleCounter.peopleCount >= fertilityUpgrade1Cost)
+        if(peopleCounter.peopleCount >= fertilityUpgrade1Cost +2)
         {
             fertilityUpgrade1Level += 1;
             peopleCounter.AddToCount(-fertilityUpgrade1Cost);
-            fertilityUpgrade1Cost = (double)Mathf.Pow(100, fertilityUpgrade1Level);
+            fertilityUpgrade1Cost = (double)Mathf.Pow(100, fertilityUpgrade1Level+1);
 
             StopCoroutine(autoPpl);
-            autoPpl = StartCoroutine(peopleCounter.RepeatAdd(1, 5/fertilityUpgrade1Level));
+            autoPpl = StartCoroutine(peopleCounter.RepeatAdd((double)(1*Mathf.Pow(2, litterUpgradeLevel)), 5/(fertilityUpgrade1Level+1)));
         }
-        fertilityUpgrade1Text.text = $"Upgrade Fertility | Cost: {fertilityUpgrade1Cost} people";
+        fertilityUpgrade1Text.text = $"Upgrade Fertility\nCost: {fertilityUpgrade1Cost} people";
+        fertilityUpgradePanelText.text = $"This upgrade divides the time it takes for humans to automatically make babies. Lvl: {fertilityUpgrade1Level}";
+    }
+
+    void LitterUpgradeClicked()
+    {
+        if(litterUpgradeLevel == 0) 
+        {
+            litterUpgradeCost = 250;
+        } else
+        {
+            litterUpgradeCost = (double)Mathf.Pow(250*litterUpgradeLevel, 2);
+        }
+
+        if(peopleCounter.peopleCount >= litterUpgradeCost +2)
+        {
+            litterUpgradeLevel += 1;
+            peopleCounter.AddToCount(-litterUpgradeCost);
+            litterUpgradeCost = (double)Mathf.Pow(250*litterUpgradeLevel, 2);
+
+            StopCoroutine(autoPpl);
+            autoPpl = StartCoroutine(peopleCounter.RepeatAdd((double)(1*Mathf.Pow(2, litterUpgradeLevel)), 5/(fertilityUpgrade1Level+1)));
+        }
+        litterUpgradeText.text = $"Double Litter Size\nCost: {litterUpgradeCost} people";
+        litterUpgradePanelText.text = $"This upgrade doubles the amount of babies made per level. Grows exponentially. Lvl: {litterUpgradeLevel}";
+
+        forceReproduceText.text = $"Force Reproduction\n(+{(double)(1*Mathf.Pow(2, litterUpgradeLevel))} people)";
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    { 
+    {
         button1.onClick.AddListener(StartButtonClicked);
         forceReproduceButton.onClick.AddListener(ForceReproduceClicked);
         fertilityUpgrade1.onClick.AddListener(FertilityUpgradeClicked);
+        litterUpgrade.onClick.AddListener(LitterUpgradeClicked);
 
         forceReproduceButton.gameObject.SetActive(false);
         fertilityUpgrade1.gameObject.SetActive(false);
-
+        litterUpgrade.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
